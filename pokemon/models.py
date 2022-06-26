92e44ea75dd60dd2cac5ec9f13d15d51a83ddf96
PhilipZara <80489788+PhilipZara@users.noreply.github.com>
2022-06-27 02:38:46 +0300

    Add files via upload

--- pokemon/models.py
+++ pokemon/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Pokedex(models.Model):
     name = models.CharField(max_length=200)
-    abilities = models.CharField(max_length=200)
-    stats = models.CharField(max_length=200)
+    abilities = models.JSONField()
+    stats = models.JSONField()
     sprite = models.CharField(max_length=300)
 
     def __str__(self):
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,41 +1,45 @@
 from django.shortcuts import render
 from .models import Pokedex
 import requests
+from django.contrib import messages
 
 
 def pokemon_crawler():
-    pokemon_id = 1
-    while pokemon_id >= 1:
-        ability_names = ""
-        poke_stats = ""
-        response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/")
+    # The link below is the one that the API suggests of using to get all Pokemons.
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0").json()
+    pokemons = response["results"]
+    for poke in pokemons:
+        response = requests.get(poke["url"])
         if response.status_code == 404:
-            break
+            continue
         else:
             name = response.json()["name"]
 
             abilities = response.json()["abilities"]
-            for ability in abilities:
-                ability_names += ability["ability"]["name"] + "\n"
 
             stats = response.json()["stats"]
-            for stat in stats:
-                stat_base = stat["base_stat"]
-                stat_name = stat["stat"]["name"]
-                poke_stats += stat_name + ": " + str(stat_base) + "\n"
 
             sprite = response.json()["sprites"]["front_default"]
-            if Pokedex.objects.filter(name__contains=name):
-                Pokedex.objects.filter(name__contains=name).update(abilities=ability_names, stats=poke_stats, sprite=sprite)
+
+            ability_names = [ability["ability"]["name"] for ability in abilities]
+
+            poke_stats = [stat["stat"]["name"] + ": " + str(stat["base_stat"]) for stat in stats]
+
+            if Pokedex.objects.filter(name=name):
+                Pokedex.objects.filter(name=name).update(abilities=ability_names, stats=poke_stats, sprite=sprite)
             else:
-                Pokedex.objects.create(name=name, abilities=ability_names, stats=poke_stats, sprite=sprite)
-            pokemon_id += 1
+                if name or ability_names or poke_stats or sprite is None:
+                    continue
+                else:
+                    Pokedex.objects.create(name=name, abilities=ability_names, stats=poke_stats, sprite=sprite)
 
 
 def home(request):
+    pokemons = Pokedex.objects.all()
     if request.GET.get('Update Database') == 'Update Database':
         pokemon_crawler()
-    return render(request, 'home.html', {})
+        messages.success(request, "Your database has been updated!")
+    return render(request, 'home.html', {'pokemons': pokemons})
 
 
 def pokemon(request):
@@ -43,10 +47,8 @@ def pokemon(request):
         searched = request.POST["searched"].lower()
         if Pokedex.objects.filter(name=searched):
             pokedex = Pokedex.objects.get(name=searched)
-            abilities = pokedex.abilities.rsplit("\n")
-            abilities.pop()
-            stats = pokedex.stats.rsplit("\n")
-            stats.pop()
+            abilities = pokedex.abilities
+            stats = pokedex.stats
             return render(request, 'pokemon.html', {"pokedex": pokedex,
                                                     "abilities": abilities,
                                                     "stats": stats})
